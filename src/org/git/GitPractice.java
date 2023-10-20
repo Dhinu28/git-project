c440683fa4d1a1c011d30f8d63559355f3183e21
Dhinu28 <g.dhinakaran28@gmail.com>
2023-10-20 15:07:11 +0530

    new Dhinakaran branch

--- src/org/git/GitPractice.java
+++ src/org/git/GitPractice.java
@@ -1,5 +1,10 @@
 package org.git;
 
 public class GitPractice {
+	public static void main(String[] args) {
+		System.out.println("hello this is my git project ");
+		System.out.println("this is dhinakaran branch");
+		System.out.println("this is my simple code");
+	}
 
 }
